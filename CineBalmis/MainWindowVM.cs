1e18f3ee6947db7b8d5702cab6f7598e930d19a8
alumno <alumno@A302PC35>
2024-02-15 10:44:15 +0100

    Boton Salir e Imagen Fondo

--- CineBalmis/MainWindowVM.cs
+++ CineBalmis/MainWindowVM.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CineBalmis
 {
-    internal class MainWindowVM
+    internal class MainWindowVM : ObservableObject
     {
+        private string empleado;
+        public string Empleado
+        {
+            get { return empleado; }
+            set { SetProperty(ref empleado, value); }
+        }
+
+        public MainWindowVM()
+        {
+            Empleado = "Hola";
+        }
     }
 }
